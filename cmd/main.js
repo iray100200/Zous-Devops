a9c31b41e869cfa145890b56a09bdb1924576917
iray100200 <iray100200@hotmail.com>
2020-09-19 20:51:25 +0800

    remove logic to main

---
+++ cmd/main.js
@@ -0,0 +1,53 @@
+const inquirer = require('inquirer')
+const { Command } = require('commander')
+const program = new Command()
+const fs = require('fs')
+const packageJson = JSON.parse(fs.readFileSync('package.json'))
+const Spawn = require('../scripts/spawn').default
+const checkEnv = require('../scripts/checkEnv').default
+const ssh = require('./ssh')
+const Spinner = require('../utils/console/spinner-progress')
+program.version(packageJson.version)
+
+const promptList = [
+  {
+    type: 'input',
+    message: 'Test command: ',
+    name: 'content',
+    required: true,
+    default: "npm --version",
+    validate: function (answer) {
+      if (!answer) {
+        return false
+      }
+      return true
+    } 
+  }
+]
+
+program.command('spinner')
+  .action(function () {
+    const spinner = new Spinner()
+    spinner.setText('running...')
+    spinner.start()
+  })
+
+program.command('ssh')
+  .action(function () {
+    ssh()
+  })
+
+program
+  .command('test')
+  .action(function () {
+    inquirer.prompt(promptList).then(answer => {
+      Spawn.runScript(answer.content)
+    })
+  })
+
+program.command('env')
+  .action(function () {
+    checkEnv()
+  })
+
+program.parse(process.argv)
